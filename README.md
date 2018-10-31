c4458ed2fe8707a58170187d91739c4b7eb15ddd
Eden Synrez <esynr3z@gmail.com>
2018-10-31 07:55:57 +0300

    Update documentation

--- README.md
+++ README.md
@@ -1,25 +1,25 @@
 # USB 2.0 FS Device controller IP core
 
-**NOT STABLE - CONSISTENCE BETWEEN THE CODE/DESCRIPTION/OTHER INFORMATION MAY BE BROKEN**
+**UNDER DEVELOPMENT - CONSISTENCE BETWEEN THE CODE/DESCRIPTION/OTHER INFORMATION MAY BE BROKEN**
 
 **usb20dev** is an open source IP core written in SystemVerilog for interfacing USB without any external chips.
-The main idea is just to route USB signals directly to FPGA and use usual tri-state IO as "analog" frontend. Yes, this will be outside of the USB specs, but should work. And USB 2.0 full-speed (12Mbps) is the maximum we can achieve by this way.
+The main idea is just to route USB signals directly to FPGA and use a pair of tri-state IO as "analog" frontend. Yes, this will be outside of the USB specs, but should work. And USB 2.0 full-speed (12Mbps) is the maximum can be achieved this way.
 
 ## Functional blocks
 
 ![func_sch](doc/func_sch.png)
 
 * **"Analog" Frontend (FE)** : Two generic 3.3V tri-state IO
-* **USB 2.0 Transceiver Macrocell (UTM)** :
-    * Bit stuffing / unstuffing
+* **Serial Interface Engine (SIE)** :
     * NRZI encoding / decoding
-    * SYNC and EOP handling
+    * Bit stuffing / unstuffing
     * Serial-Parallel / Parallel-Serial Conversion
-* **Protocol Engine (PE)** :
+    * SYNC, RESET and EOP detection
+* **[Not implemented yet] Protocol Engine (PE)** :
     * Packet recognition
     * Transaction sequencing
     * CRC generation and checking
     * Packet ID (PID) generation and checking / decoding
-* **Endpoints Router (EPR)** : Interface for endpoints
-* **Control Endpoint (EP0)** : Endpoint 0 IN and Endpoint 0 OUT logic
-* **Device Specific Endpoinds** : All other user application endpoints
+* **[Not implemented yet] Endpoints Router (EPR)** : Interface for endpoints array
+* **[Not implemented yet] Control Endpoint (EP0)** : Endpoint 0 IN and Endpoint 0 OUT logic
+* **[Not implemented yet] Device Specific Endpoinds** : All other user application endpoints
--- rtl/usb_sie_if.sv
+++ rtl/usb_sie_if.sv
@@ -16,10 +16,46 @@ logic             tx_valid;     // Data on tx_data bus is valid
 logic             tx_ready;     // SIE ready to load transmit data into holding registers
 logic             tx_active;    // Transmit state machine is active (from SYNC sending start to EOP sending end)
 
+// Example of packet transmission
+//                 .---------------------------------.
+//  tx_valid       |                                 |
+//            -----'                                 '-----------------------
+//            ----. .--. .--------------. .----------------------------------
+//   tx_data   xxx X B0 X  BYTE1         X  BYTE2
+//            ----' '--' '--------------' '----------------------------------
+//                    .-.              .-.       .-.
+//  tx_ready          | |              | |       | |
+//            --------' '--------------' '-------' '-------------------------
+//                        .-----------------------------------------------.
+// tx_active              |                                               |
+//            ------------'                                               '--
+//                          .-------. .-------. .-------. .-------. .---.
+//    D+/D-   ----IDLE-----<  SYNC   X  BYTE0  X  BYTE1  X  BYTE2  X EOP >---
+//                          '-------' '-------' '-------' '-------' '---'
+
 bus8_t            rx_data;      // Received USB data
 logic             rx_valid;     // rx_data bus has valid data
 logic             rx_active;    // Receive state machine is active (from detecting SYNC to detecting EOP)
 logic             rx_error;     // Receive error detection (bitstuff error)
+
+// Example of packet reception
+//                     .-------. .-------. .-------. .-------. .---.
+//     D+/D-  --IDLE--<  SYNC   X  BYTE0  X  BYTE1  X  BYTE2  X EOP >--IDLE--
+//                     '-------' '-------' '-------' '-------' '---'
+//                               .-----------------------------------.
+// rx_active                     |                                   |
+//            -------------------'                                   '-------
+//            ---------------------------. .-------. .-------. .-------------
+//                          xxx           X  BYTE0  X  BYTE1  X  BYTE2
+//   rx_data  ---------------------------' '-------' '-------' '-------------
+//                                        .-.       .-.       .-.
+//                                        | |       | |       | |
+//  rx_valid  ----------------------------' '-------' '-------' '------------
+//                                                      .-.
+//                                                      | |
+//  rx_error  ------------------------------------------' '------------------
+//  NOTE: rx_error can be asserted in any time bitstuff error occured.
+//        PE should ignore the whole packet after that.
 
 // Serial Interface Engine side
 modport sie (
