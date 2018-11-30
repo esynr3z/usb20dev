b74c6ac39dd7fb657c59342efb22cdc7818396c4
Eden Synrez <esynr3z@gmail.com>
2018-11-30 20:55:09 +0300

    Place SIE dut inside tb file

--- verif/testbenches/sie/tb.sv
+++ verif/testbenches/sie/tb.sv
@@ -6,7 +6,29 @@
 //==============================================================================
 
 `include "../testbenches/tb_header.svh"
-`include "../testbenches/tb_dut_sie.svh"
+
+//-----------------------------------------------------------------------------
+// DUT top
+//-----------------------------------------------------------------------------
+usb_fe_if usb_fe();
+usb_sie_if usb_sie_bus();
+
+usb_sie dut (
+    .clk     (tb_clk),
+    .rst     (~tb_rst_n),
+    .fe_ctrl (usb_fe.ctrl),
+    .sie_bus (usb_sie_bus.sie)
+);
+
+usb_host_beh host_beh (
+    .phy (usb_fe.phy)
+);
+
+usb_sie_vip sie_vip (
+    .clk     (tb_clk),
+    .rst     (~tb_rst_n),
+    .sie_bus (usb_sie_bus.pe)
+);
 
 //`define STOP_TIME  100ms   // Time when test stops
 `define TEST_DESCR "SIE test - receive, transmit and special cases"
